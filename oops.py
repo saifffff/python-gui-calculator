3385b15b3bbd02b1a73089262032e58f1a3509b8
saifffff <angryn1nj4@gmail,com>
2022-04-20 22:14:00 +0530

    calculator started

--- oops.py
+++ oops.py
@@ -18,15 +18,22 @@ class Player:
     @classmethod
     def createInstance(cls,string):
         details = string.split("-")
-        return cls(details[0],details[1],details[2],details[3])
+        # return cls(details[0],details[1],details[2],details[3])
+        return cls(*string.split("-"))
+    # static methods are used when we do not need class or even self(objects) in the processing
+    @staticmethod
+    def useless(string):
+        return string
 
 ronaldo = Player("Cristiano","Manchester United","Portugal",7)
 messi = Player.createInstance("Lionel-PSG-Argentina-10")
 
-print(messi.printDetails())
+# print(messi.printDetails())
+# print(messi.useless("hi i am useless"))
 
+class Sportsman(Player):
+    pass
 
+kaka = Sportsman.createInstance("Kaka-Barca-Brasil-10")
 
-
-
-
+print(kaka.printDetails())
