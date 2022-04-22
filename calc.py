24b3028375ecbf4aa56f9fab54ed71ffe59102e1
saifffff <angryn1nj4@gmail,com>
2022-04-22 21:02:03 +0530

    tweaks

--- calc.py
+++ calc.py
@@ -27,17 +27,21 @@ justify = LEFT).grid(columnspan = 4 , ipadx=120)
 
 def changeAns_val(entry):
     global ansValGlobal
-    global ansLabelGlobal
-    
+    global ansLabelGlobal 
     ansValGlobal = ansValGlobal + str(entry)
     ansLabelGlobal = ansValGlobal
     Ans_val.set(ansValGlobal)
+
+
 def clearAns_val():
     global ansValGlobal
     global ansLabelGlobal
     ansLabelGlobal = ansValGlobal
     ansValGlobal = ""
     Ans_val.set(ansValGlobal)
+
+
+
 def evaluateSquareRoot():
     global ansValGlobal
     global ansLabelGlobal
@@ -53,17 +57,21 @@ def evaluateSquareRoot():
         except(ValueError,SyntaxError,TypeError,ZeroDivisionError):
             clearAns_val()
             answerFinalLabel.set("Error!")
+
+
 def evaluateAnswer():
     global ansValGlobal
     try:
        eval(ansValGlobal)
        evaluatedValueAnswerLabelGlobal= str(eval(ansValGlobal))
-        # This line should be alligned properly without any indentation error
        clearAns_val()
        answerFinalLabel.set(evaluatedValueAnswerLabelGlobal)
     except(ValueError,SyntaxError,TypeError, ZeroDivisionError):
         clearAns_val()
         answerFinalLabel.set("Error!")
+
+
+
 def allClear():
     global ansValGlobal
     global ansLabelGlobal
@@ -71,13 +79,19 @@ def allClear():
     ansLabelGlobal = ""
     Ans_val.set("")
     answerFinalLabel.set("")
+
+
+
 def createButton(txt,x,y):
     Button(root, font=('futura', 15, 'bold'),
            padx=16,pady=16,text = str(txt),
            command = lambda:changeAns_val(txt),
            height = 2, width=9).grid(row = x , column = y, sticky=E)
+
+
 buttons = ['AC','√','%','/','7','8','9','*','4','5','6','-','1','2','3','+','','','.','']
 buttonsListTraversalCounter = 0
+
 for i in range(3,8):
     for j in range(0,4):
         createButton(buttons[buttonsListTraversalCounter],i,j)
@@ -87,10 +101,14 @@ Button(root,font=('futura', 15, 'bold'),
        padx=16,pady=16, text = "√",
        command = lambda:evaluateSquareRoot(),
        height=2, width=9).grid(row = 3 , column = 1, sticky = E)
+
+
+
 Button(root,font=('futura', 15, 'bold'),
        padx=16,pady=16, text = "AC",
        command = lambda:allClear(),
        height=2, width=9).grid(row = 3 , column = 0 , sticky = E)
+       
 
 Button(root,font=('futura', 15, 'bold'),
        padx=16,pady=16, text = "0",
